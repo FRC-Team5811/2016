b09fe79c9f475d59057f1881fe140e87637ca0f6
CraftyARL <aaron.rlewis26@gmail.com>
2016-02-10 19:07:29 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team5811.robot;
 
 
@@ -6,7 +5,7 @@ package org.usfirst.frc.team5811.robot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-//import org.usfirst.frc.team5811.robot.commands.ExampleCommand;
+import org.usfirst.frc.team5811.robot.commands.ExampleCommand;
 import org.usfirst.frc.team5811.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -43,6 +42,9 @@ public class Robot extends IterativeRobot {
     double leftSpeed;
     double rightSpeed;
     
+    //A cylinder
+    DoubleSolenoid cylinder;
+    
     //COMPRESSOR!!!
     Compressor compressor;
     
@@ -59,6 +61,7 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     private void driveMotors(double speedLeftDM, double speedRightDM) {
+    	System.out.println("Command: " + speedLeftDM);
     	frontLeftDriveMotor.set(speedLeftDM);
     	frontRightDriveMotor.set(speedRightDM);
     	backLeftDriveMotor.set(speedLeftDM);
@@ -74,14 +77,16 @@ public class Robot extends IterativeRobot {
   
         //Motor port init
        frontLeftDriveMotor = new Victor(0);
-       frontRightDriveMotor = new Victor(1);
-       backLeftDriveMotor = new Victor(2);
+       frontRightDriveMotor = new Victor(2);
+       backLeftDriveMotor = new Victor(1); 
        backRightDriveMotor = new Victor(3);
        
        joyStickLeft = new Joystick(0);
        joyStickRight = new Joystick(1);
        
        button = new JoystickButton(joyStickLeft, 1);
+       
+       cylinder = new DoubleSolenoid(0,1);
        
        
        //set cycle counter
@@ -90,6 +95,8 @@ public class Robot extends IterativeRobot {
        //compressor port init
        compressor = new Compressor(0);
        compressor.setClosedLoopControl(false);
+       
+       
        
        //limit switch init
        limitSwitch =  new DigitalInput(1);
@@ -101,9 +108,9 @@ public class Robot extends IterativeRobot {
     
     private void operatorControl(){
     	//call if limit switch is used
-    	while(limitSwitch.get()) {
+    	/*while(limitSwitch.get()) {
     		System.out.println("limit switch");
-    	}
+    	}*/
     }
     
     public void disabledInit(){
@@ -118,7 +125,7 @@ public class Robot extends IterativeRobot {
     public void autonomousInit() {
         autonomousCommand = (Command) chooser.getSelected();
         
-        driveMotors(0,0);
+        //driveMotors(0,0);
 		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
 		switch(autoSelected) {
 		case "My Auto":
@@ -141,21 +148,22 @@ public class Robot extends IterativeRobot {
        
         cycleCounter++;
 
-        if (cycleCounter < 100) {
-        	driveMotors(1,0);
+        //driveMotors(cycleCounter/500,0);
+        /*if (cycleCounter < 100) {
+        	driveMotors(.5,0);
         }
         else if (cycleCounter < 200) {
         	driveMotors(0,0);
         }
         else if (cycleCounter < 300) {
-        	driveMotors(1,0);
+        	driveMotors(-.5,0);
         }
         else if (cycleCounter < 400) {
         	driveMotors(0,0);
         }
         else if (cycleCounter < 500) {
-        	driveMotors(1,0);
-        }
+        	driveMotors(.5,0);
+        }*/
     }
 
     public void teleopInit() {
@@ -163,21 +171,30 @@ public class Robot extends IterativeRobot {
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
         // this line or comment it out.
+    	cylinder.set(DoubleSolenoid.Value.kReverse);
         if (autonomousCommand != null) autonomousCommand.cancel();
         compressor.setClosedLoopControl(true);
     }
     	
     public void teleopPeriodic() {
+        System.out.println("Before: " + frontLeftDriveMotor.get());
         Scheduler.getInstance().run();
         
         //for driving/tank drive
-        driveMotors(joyStickLeft.getY(),0);
+        driveMotors(joyStickLeft.getY(),-1*joyStickRight.getY());
+        System.out.println("After: " + frontLeftDriveMotor.get());
         
         //checking for button values
         //change when button is ready for use
-        System.out.println(button.get());
+        //System.out.println(button.get());
+        if(button.get()){
+        	cylinder.set(DoubleSolenoid.Value.kForward);
+        }else{
+        	cylinder.set(DoubleSolenoid.Value.kReverse);
+        }
         
         operatorControl();
+        System.out.println("After 2: " + frontLeftDriveMotor.get());
     }
     
     
